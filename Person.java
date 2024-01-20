bfc6568d554ad7f802a27ac9f357507dbac1a1f0
1U23CA007AKSHAYKRISHNAN <139437417+1U23CA007AKSHAYKRISHNAN@users.noreply.github.com>
2024-01-20 04:15:02 +0000

    ss3

--- Person.java
+++ Person.java
@@ -19,8 +19,8 @@ class Person
     String ABC=a1.getfullname();
     String EFG=a2.getfullname();
 
-    System.out.println("Person 1:"+ABC);
-    System.out.println("Person 2:"+EFG);
+    System.out.println("Person 1:"+a1.getfullname());
+    System.out.println("Person 2:"+a2.getfullname());
 
     double average = ((a1.age + a2.age) /2.0);
     System.out.println("Average Age:"+average);
